cd0c7d22fa320b9bad65780657294abdc8a70610
D <85412836+dafydddev@users.noreply.github.com>
2026-07-10 00:32:47 +0100

    Simple menu system for main menu

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+    public class MenuController : MonoBehaviour
+    {
+        // The menu panels know to the scene
+        [SerializeField] private MenuPanel[] menuPanels;
+
+        // The default panel to show on scene load
+        [SerializeField] private MenuPanel defaultPanel;
+
+        // The current panel being displayed
+        private MenuPanel _currentPanel;
+        
+        private void Awake() => UpdateMenu(defaultPanel);
+
+        private void UpdateMenu(MenuPanel panel)
+        {
+            // Early exit when there are no menu panels
+            if (menuPanels == null || menuPanels.Length == 0) return;
+            // Hide all the menu panels know to the script
+            ShowPanel(panel);
+        }
+
+
+        public void ShowPanel(MenuPanel panel)
+        {
+            // Early exit when there is no panel, or it is already the current panel
+            if (!panel || panel == _currentPanel) return;
+            // Hide all the other panels
+            HideAll();
+            // Set the panel to be active
+            panel.SetActive(true);
+            // Set the panel as the active one
+            _currentPanel = panel;
+        }
+
+        private void HideAll()
+        {
+            // Early exit when there are no menu panels
+            if (menuPanels == null || menuPanels.Length == 0) return;
+            // Loop through the panels and set them all to inactive
+            foreach (var panel in menuPanels) panel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/MenuPanel.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu
+{
+    public class MenuPanel : MonoBehaviour
+    {
+        // The first selectable on the menu, setting focus onto it, helps with keyboard navigation
+        [SerializeField] private Selectable firstSelectable;
+
+        public void SetActive(bool isActive)
+        {
+            // Set the active state of the game object
+            gameObject.SetActive(isActive);
+            // If the game object is active and the first button is not null, set focus onto it
+            if (isActive && firstSelectable) firstSelectable.Select();
+        }
+    }
+}
