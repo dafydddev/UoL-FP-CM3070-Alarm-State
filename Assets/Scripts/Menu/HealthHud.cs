6ab429f22eb6bc06636cfeecfd2c76a28a69bd1d
D <85412836+dafydddev@users.noreply.github.com>
2026-07-18 23:04:45 +0100

    Added basic health system

---
+++ Assets/Scripts/Menu/HealthHud.cs
@@ -0,0 +1,28 @@
+using Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu
+{
+    // The strip of hearts on the HUD showing the player's remaining health.
+    public class HealthHud : MonoBehaviour
+    {
+        // One image per possible heart, left to right; hearts beyond the player's maximum stay hidden.
+        [SerializeField] private Image[] hearts;
+        [SerializeField] private Sprite fullHeart;
+        [SerializeField] private Sprite emptyHeart;
+
+        private void OnEnable() => PlayerHealth.OnHealthChanged += Redraw;
+        private void OnDisable() => PlayerHealth.OnHealthChanged -= Redraw;
+
+        // Full hearts for the health remaining, empty for the hearts lost.
+        private void Redraw(int current, int max)
+        {
+            for (var i = 0; i < hearts.Length; i++)
+            {
+                hearts[i].gameObject.SetActive(i < max);
+                hearts[i].sprite = i < current ? fullHeart : emptyHeart;
+            }
+        }
+    }
+}
