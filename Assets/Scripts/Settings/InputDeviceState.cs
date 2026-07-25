c4a00a582944c572498e2a05ec20e5e3ccbeb8d7
D <85412836+dafydddev@users.noreply.github.com>
2026-07-25 16:43:39 +0100

    Created input state for detecting input changes

---
+++ Assets/Scripts/Settings/InputDeviceState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+namespace Settings
+{
+    public class InputDeviceState : MonoBehaviour
+    {
+        public InputDevice CurrentDevice { get; private set; } = Keyboard.current;
+        
+        public event Action<InputDevice> InputTypeChanged;
+        
+        private void OnEnable() => InputSystem.onEvent += OnInputEvent;
+        private void OnDisable() => InputSystem.onEvent -= OnInputEvent;
+
+        private void OnInputEvent(InputEventPtr eventPtr, InputDevice device)
+        {
+            var inputDevice = device switch
+            {
+                Gamepad => Gamepad.current,
+                Keyboard => Keyboard.current,
+                _ => CurrentDevice
+            };
+
+            if (inputDevice == CurrentDevice) return;
+
+            CurrentDevice = inputDevice;
+            InputTypeChanged?.Invoke(inputDevice);
+        }
+    }
+}
