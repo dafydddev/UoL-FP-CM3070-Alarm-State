ffabecf7678508abd395375dde7367b8e656fde3
D <85412836+dafydddev@users.noreply.github.com>
2026-07-31 16:33:51 +0100

    Made generic cell surface so guards and lasers can use a shared system

--- Assets/Scripts/Guards/GuardVisionField.cs
+++ Assets/Scripts/Guards/GuardVisionField.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Effects;
 using UnityEngine;
 
 namespace Guards
@@ -29,14 +30,14 @@ namespace Guards
         private readonly HashSet<Vector3> _calmCells = new();
         private readonly HashSet<Vector3> _alertCells = new();
 
-        private Surface _calm;
-        private Surface _alert;
+        private CellSurface _calm;
+        private CellSurface _alert;
         private Vector2 _cellSize = Vector2.one;
 
         private void Awake()
         {
-            _calm = new Surface(transform, "CalmVision", sortingOrder);
-            _alert = new Surface(transform, "AlertVision", sortingOrder);
+            _calm = new CellSurface(transform, "CalmVision", sortingOrder);
+            _alert = new CellSurface(transform, "AlertVision", sortingOrder);
         }
 
         private void LateUpdate()
@@ -103,70 +104,6 @@ namespace Guards
         {
             _calm?.Dispose();
             _alert?.Dispose();
-        }
-
-        // One translucent mesh of camera-facing quads, one per painted cell.
-        private sealed class Surface
-        {
-            private readonly Transform _root;
-            private readonly MeshRenderer _renderer;
-            private readonly Material _material;
-            private readonly Mesh _mesh;
-            private readonly List<Vector3> _verts = new();
-            private readonly List<int> _tris = new();
-
-            public Surface(Transform parent, string name, int sortingOrder)
-            {
-                var go = new GameObject(name);
-                _root = go.transform;
-                _root.SetParent(parent, false);
-
-                _mesh = new Mesh { name = name };
-                _mesh.MarkDynamic();
-                go.AddComponent<MeshFilter>().mesh = _mesh;
-
-                // Sprites/Default is an always-included unlit shader that respects sorting order
-                // and premultiplies alpha, so a translucent _Color blends cleanly over the tiles.
-                _material = new Material(Shader.Find("Sprites/Default"));
-                _renderer = go.AddComponent<MeshRenderer>();
-                _renderer.sharedMaterial = _material;
-                _renderer.sortingOrder = sortingOrder;
-            }
-
-            public void SetVisible(bool visible) => _renderer.enabled = visible;
-
-            public void Rebuild(HashSet<Vector3> centers, Vector2 half, Color color)
-            {
-                _material.color = color;
-                _verts.Clear();
-                _tris.Clear();
-
-                foreach (var center in centers)
-                {
-                    var c = _root.InverseTransformPoint(center);
-                    var i = _verts.Count;
-                    _verts.Add(c + new Vector3(-half.x, -half.y));
-                    _verts.Add(c + new Vector3(-half.x, half.y));
-                    _verts.Add(c + new Vector3(half.x, half.y));
-                    _verts.Add(c + new Vector3(half.x, -half.y));
-                    _tris.Add(i);
-                    _tris.Add(i + 1);
-                    _tris.Add(i + 2);
-                    _tris.Add(i);
-                    _tris.Add(i + 2);
-                    _tris.Add(i + 3);
-                }
-
-                _mesh.Clear();
-                _mesh.SetVertices(_verts);
-                _mesh.SetTriangles(_tris, 0);
-            }
-
-            public void Dispose()
-            {
-                if (_mesh) Destroy(_mesh);
-                if (_material) Destroy(_material);
-            }
         }
     }
 }
